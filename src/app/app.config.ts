a64f4fc3622281849838a2264096b82f6a7c3cd5
Jose Matos <developer@josematos.work>
2024-10-30 00:37:24 +0100

    Update project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
   ],
 };
