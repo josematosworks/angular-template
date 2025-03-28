92bc619cdbee9b66b28eaa88db8ce0abc309af34
José Matos <developer@josematoswork.com>
2025-03-28 12:13:59 +0100

    chore: add known issues file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,8 @@
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import type { Preview } from '@storybook/angular';
 
-// Import the Compodoc generated documentation using require
-// This avoids TypeScript issues with JSON imports
 const docJson = require('../documentation.json');
 setCompodocJson(docJson);
-
-// The styles will be loaded by the component itself
 
 const preview: Preview = {
   parameters: {
---
+++ KNOWN-ISSUES.md
@@ -0,0 +1,6 @@
+# KNOWN-ISSUES
+
+## Storybook
+
+- TailwindCSS is not working in Storybook
+- PrimeNG styles are not working in Storybook
